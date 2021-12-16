67dc206d8301bea48c14314bb68ff399fae4f3c8
dust1 <834902408@qq.com>
2021-12-16 16:13:04 +0800

    simple disk manager

--- src/storage/relational/mod.rs
+++ src/storage/relational/mod.rs
@@ -1,8 +1,11 @@
-pub mod buffer_pool;
-
+mod buffer_pool;
 mod clock_replacer;
 mod disk_manager;
 mod page;
+mod relational;
+mod tuple;
+
+#[cfg(test)]
+mod buffer_pool_test;
 #[cfg(test)]
 mod page_test;
-mod tuple;
